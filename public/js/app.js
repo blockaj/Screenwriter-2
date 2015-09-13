e3ac402a17f3c55e49219565ddea611849c5961f
Aaron <ajblock@gmail.com>
2015-09-13 17:37:37 -0400

    Succesful scene heading, but that's it

--- public/js/app.js
+++ public/js/app.js
@@ -1,42 +1,75 @@
 var React = require("react");
 
+var ScreenplayElement = React.createClass({
+	handleKeyPress: function(e) {
+		if (e.key === "Enter") {
+			e.preventDefault();
+			if (this.props.type == "scene-heading") {
+
+			}
+			else if (this.props.type == "action") {
+				console.log("New action to be created");
+			}
+			else if (this.props.type == "character") {
+				console.log("New dialogue to be created");
+			}
+			else if (this.props.type == "dialogue") {
+				console.log("New action to be created");
+			}
+		}
+	},
+	handleKeyDown: function(e) {
+		if (e.key == "Tab") {
+			e.preventDefault();
+			console.log("Change something");
+		}
+	},
+	render: function () {
+		var classString = this.props.type + " screenplay-el";
+
+		return (
+			<p className={classString} contentEditable onKeyPress={this.handleKeyPress} onKeyDown={this.handleKeyDown}><br /></p>
+		);
+	}
+});
+
 var SceneHeading = React.createClass({
 	render: function() {
 		return (
-			<div className="scene-heading"><br /></div>
-		)
+			<ScreenplayElement type="scene-heading" />
+		);
 	}
 });
 
 var Action = React.createClass({
 	render: function() {
 		return (
-			<div className="action"><br /></div>
-		)
+			<ScreenplayElement type="action" />
+		);
 	}
 });
 
 var Character = React.createClass({
 	render: function() {
 		return (
-			<div className="character"><br /></div>
-		)
+			<ScreenplayElement type="character" />
+		);
 	}
 });
 
 var Dialogue = React.createClass({
 	render: function() {
 		return (
-			<div className="dialogue"><br /></div>
-		)
+			<ScreenplayElement type="dialogue" />
+		);
 	}
 })
 
 var DocumentBody = React.createClass({
 	render: function() {
 		return (
-			<div className="writable-content" contentEditable="true"><SceneHeading /></div>
-		)
+			<div className="writable-content"><SceneHeading /></div>
+		);
 	} 
 });
 	
