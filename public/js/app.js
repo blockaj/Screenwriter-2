3b92055dd207dbdba23c7423c6755daf94849258
Aaron <ajblock@gmail.com>
2015-10-13 17:30:10 -0400

    Committing so I can pull

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,11 @@
 var React = require("react");
 
-var elementList = ["Hello, world!", "Pop!"];
+var elementList = [<ScreenplayElement elementType="scene-heading"><BR /></ScreenplayElement>];
 
 // Content-editable p-tags are not editable without some content.
 // Therefore, we must start each new ScreenplayElement with a 
 // <br /> tag which will be destroyed as soon as the user begins
 // to edit. This React component is a wrapper for the <br /> tag.
-// 
 var BR = React.createClass({
 	vileHtml: function() {
 		return {__html: "<br />"};
